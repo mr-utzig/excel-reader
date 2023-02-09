a9073124419988b9cd04df6f07c0024e0fe596dc
Mr.Utzig <lutzig.octavius@gmail.com>
2023-02-09 12:50:21 -0300

    Bootstrap install and index view config

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excel Reader</title>
+    <livewire:styles />
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <livewire:scripts />
+</body>
+
+</html>
